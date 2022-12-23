9f745264c9883aa6b2ba58f803247b677361dd7d
Hu Zhenwei <huzhenweitom@gmail.com>
2022-12-22 18:10:30 -0600

    Add ICPC problems, change logic of UserACStatus (subproblem version)

--- scripts/UpdateCF_Problems.py
+++ scripts/UpdateCF_Problems.py
@@ -3,9 +3,11 @@ import os
 import sys
 import time
 import requests
+import argparse
 
 FILE_PATH = "public/contests.json"
-INVALID_ID = {1410, 1414, 1412, 1258, 1226, 1224, 1222, 1094, 1050, 1049, 1048, 905, 885, 874, 857, 826, 728, 726, 693, 630}
+INVALID_ID = {1597, 1596, 1595, 1410, 1414, 1412, 1258, 1226, 1224, 1222, 1094, 1050, 1049, 1048, 905, 885, 874, 857, 826, 728, 726, 693, 630
+, 345, 76, 48, 38, 22, 17, 6}
 
 
 '''
@@ -55,25 +57,33 @@ def load_contest_byid(id):
     name = info["result"]["contest"]["name"]
     stripped_name = "".join(name.split())
 
+    problem_list = info["result"]["problems"]
+    problem_list = list(map(process, problem_list))
+    index_set = set()
+    for problem in problem_list:
+        if len(problem["index"]) > 1:
+            index_set.add(problem["index"][0])
+        else:
+            index_set.add(problem["index"])
+    unique_problem_cnt = len(index_set)
+
     type = "Others"
     if "Global" in stripped_name:
         type = "Global"
     elif "Educational" in stripped_name:
         type = "Educational"
+    elif unique_problem_cnt >= 10:
+        type = "ICPC"
     elif "Div.1+Div.2" in stripped_name:
         type = "Div1 + Div2"
-    elif "Div.1" in stripped_name:
+    elif "Div.1" in stripped_name and unique_problem_cnt <= 8:
         type = "Div1"
-    elif "Div.2" in stripped_name:
+    elif "Div.2" in stripped_name and unique_problem_cnt <= 8:
         type = "Div2"
     elif "Div.3" in stripped_name:
         type = "Div3"
     elif "Div.4" in stripped_name:
         type = "Div4"
-
-
-    problem_list = info["result"]["problems"]
-    problem_list = list(map(process, problem_list))
 
     if len(problem_list) == 0 or any("rating" not in x for x in problem_list):
         return None
@@ -84,12 +94,14 @@ def load_contest_byid(id):
             if (problem_result[i]["points"] > 0):
                 problem_list[i]["attempted"] += 1 + problem_result[i]["rejectedAttemptCount"]
                 problem_list[i]["solved"] += 1
-
+    
     contest_obj = dict()
     contest_obj["id"] = id
     contest_obj["type"] = type
     contest_obj["problems"] = problem_list
     contest_obj["name"] = name
+    contest_obj["problem_cnt"] = unique_problem_cnt  # only consider unique problems
+
     return contest_obj
 
 
@@ -98,11 +110,13 @@ def filter_contest_helper(contest_result):
     if contest_result["phase"] == "FINISHED":
         if contest_result["type"] == "CF":
             return True
-        if contest_result["type"] == "ICPC" and ("Div. 3" in contest_result["name"] or "Div.3" in contest_result["name"]):
-            return True
-        if contest_result["type"] == "ICPC" and ("Div. 4" in contest_result["name"] or "Div.4" in contest_result["name"]):
-            return True
-        if contest_result["type"] == "ICPC" and "Educational" in contest_result["name"]:
+        # if contest_result["type"] == "ICPC" and ("Div. 3" in contest_result["name"] or "Div.3" in contest_result["name"]):
+        #     return True
+        # if contest_result["type"] == "ICPC" and ("Div. 4" in contest_result["name"] or "Div.4" in contest_result["name"]):
+        #     return True
+        # if contest_result["type"] == "ICPC" and "Educational" in contest_result["name"]:
+        #     return True
+        if contest_result["type"] == "ICPC":
             return True
 
         return False
@@ -111,7 +125,7 @@ def filter_contest_helper(contest_result):
 
 
 
-def load_contest_all():
+def load_contest_all(args):
     try_count = 0
 
     while try_count < 5:
@@ -137,7 +151,7 @@ def load_contest_all():
             if id in INVALID_ID:  # ignore all invalid ID
                 continue
 
-            if id not in contest_id_dict:
+            if id not in contest_id_dict or args.force:
                 contest_obj = load_contest_byid(id)
                 if contest_obj:   # load the contest only if it exists
                     contest_id_dict[id] = contest_obj
@@ -146,12 +160,14 @@ def load_contest_all():
 
 
             if contest_obj:   # only append valid info
+                problem_cnt = 0
                 if "sub" not in contest_obj:
                     sub = 0
                     for problem in contest_obj["problems"]:
                         if len(problem["index"]) > 1:
                             sub = 1
                     contest_obj["sub"] = sub
+                
 
                 contest_list_all.append(contest_obj)
 
@@ -165,5 +181,8 @@ def load_contest_all():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--force", "-f", action="store_true", help="Force update all contest")  # path for images dir
+    args = parser.parse_args()
     load_contest_json()
-    load_contest_all()
+    load_contest_all(args)
